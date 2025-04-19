66502d2c63f32208a8138531c773927b1781c7bd
MD-Sabbir-Hosain-Rafi99 <sabbirhossain.rafi121@gmail.com>
2025-04-20 01:12:29 +0600

    Done

--- JS/script.js
+++ JS/script.js
@@ -4,10 +4,14 @@ let age = [24, 25, 28, 30];
 
 console.log("My name is "+teamMembarName[0]+", "+"I live in "+area[0]+", "+"I am "+age[0]+" years old.");
 
+//=========================================================================================================
+
 let numtaNumber = 5;
 for(let i=1; i <= 10; i++){
     console.log(numtaNumber+" X"+" "+i+" = "+numtaNumber*i);
 }
+
+//=========================================================================================================
 
 let teamMemberInfo = {
     membarOne:{
