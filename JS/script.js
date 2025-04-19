141c9f4562d50da38aa5f4c50c4245b1b75df639
MD-Sabbir-Hosain-Rafi99 <sabbirhossain.rafi121@gmail.com>
2025-04-19 23:47:42 +0600

    HW Done

--- JS/script.js
+++ JS/script.js
@@ -5,6 +5,22 @@ let age = [24, 25, 28, 30];
 console.log("My name is "+teamMembarName[0]+", "+"I live in "+area[0]+", "+"I am "+age[0]+" years old.");
 
 let numtaNumber = 5;
-for(let i; 1 <= 10; i++){
+for(let i=1; i <= 10; i++){
     console.log(numtaNumber+" X"+" "+i+" = "+numtaNumber*i);
 }
+
+let teamMemberInfo = {
+    membarOne:{
+        name : "Rafi",
+        age : 24,
+        area : "Dhaka",
+        hobby : ["Bike Riding", "Gaming", "Eating"]
+    },
+    membarTwo:{
+        firstName : "MD Jubair",
+        lastName : "Ahmed",
+        age : 22,
+        area : "Dhaka"
+    }
+}
+console.log(teamMemberInfo.membarOne.hobby[1]);
