51208fd1eef953aa11438883de13f5f9c08cb1eb
MD-Sabbir-Hosain-Rafi99 <sabbirhossain.rafi121@gmail.com>
2025-04-19 23:16:58 +0600

    first commit

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+let teamMembarName = ["Sabbir", "Soumik", "Ratul", "Tonmoy"];
+let area = ["Dhaka", "Barisal", "Khulna", "Manikganj"];
+let age = [24, 25, 28, 30];
+
+console.log("My name is "+teamMembarName[0]+", "+"I live in "+area[0]+", "+"I am "+age[0]+" years old.");
+
+let numtaNumber = 5;
+for(let i; 1 <= 10; i++){
+    console.log(numtaNumber+" X"+" "+i+" = "+numtaNumber*i);
+}
